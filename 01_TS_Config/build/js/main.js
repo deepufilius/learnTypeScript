1bbc9c8d8561b39013ff4fdcceb325b85106f70e
deepu filius <deepuadari@gmail.com>
2025-05-16 10:16:08 +0530

    created folder structure and configured tsconfig file

---
+++ 01_TS_Config/build/js/main.js
@@ -0,0 +1,8 @@
+"use strict";
+let userName = "Filius";
+console.log(userName);
+let a = 12;
+let b = '6';
+let c = 4;
+console.log(a / b);
+console.log(c * b);
---
+++ 01_TS_Config/src/main.ts
@@ -0,0 +1,9 @@
+let userName = "Filius";
+console.log(userName);
+
+let a = 12;
+let b = '6';
+let c = 4;
+
+console.log(a / b);
+console.log(c * b);
